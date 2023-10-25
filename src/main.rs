aa48e7475b9e52fb26c38c5fb24e538322b4853d
Andy Henshaw <andy@nexuslabs.dev>
2023-10-25 11:00:01 -0400

    Class demonstration version

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::{bail, Result};
 use clap::Parser;
-use grb::Model;
-use std::fmt;
-use std::fs::File;
-use std::io::Write;
+use good_lp::solvers::lp_solvers::{GurobiSolver, LpSolver, Model};
+use good_lp::{variable, Expression, ProblemVariables, SolverModel, Variable};
+use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
 /// A simple utility to convert standard-format Sudoku puzzles
@@ -17,12 +16,6 @@ pub struct AppArgs {
     solve: bool,
 }
 
-struct LpInfo {
-    pub objective: Vec<i64>,
-    pub b: Vec<Equality>,
-    pub constraints: Vec<Vec<(usize, i64)>>,
-}
-
 fn main() {
     let args = AppArgs::parse();
 
@@ -31,15 +24,11 @@ fn main() {
     out_file.set_extension("lp");
 
     if let Ok(puzzle) = load(&args.in_file) {
-        if let Ok(lp) = generate(&puzzle) {
-            let mut output = File::create(&out_file).expect("File I/O");
-            write!(output, "{}", lp).expect("File I/O");
+        if let Ok(model) = generate(&puzzle) {
             if args.solve {
-                let mut model = Model::new("sudoku").unwrap();
-                model.read(&(out_file.display().to_string())).unwrap();
-                model.optimize().unwrap();
-                for x in model.get_vars() {
-                    dbg!(x);
+                match model.solve() {
+                    Ok(_solution) => println!("solved"),
+                    Err(e) => eprintln!("{e}"),
                 }
             }
         }
@@ -56,7 +45,7 @@ fn load(file: &Path) -> Result<String> {
 
 /// Create LP from normalized puzzle.  Assumptions
 /// are that the puzzle is either 4x4, 6x6, or 9x9.
-fn generate(puzzle: &str) -> Result<LpInfo> {
+fn generate(puzzle: &str) -> Result<Model<GurobiSolver>> {
     let (size, box_rows, box_cols) = match puzzle.len() {
         81 => (9, 3, 3), // standard sudoku
         36 => (6, 2, 3), // 6x6, this is the only one where num box rows != box cols
@@ -64,37 +53,56 @@ fn generate(puzzle: &str) -> Result<LpInfo> {
         _ => bail!("Expected 9x9, 6x6, or 4x4 puzzle"),
     };
 
-    // lambda to compute variable index
-    let x = |row, col, value| row * size * size + col * size + value;
+    // create all the variables and store
+    // them in a HashMap for later reference
+    type Idx = (usize, usize, usize);
+    let mut problem = ProblemVariables::new();
+    let mut x: HashMap<Idx, Variable> = HashMap::new();
+    for row in 1..=size {
+        for col in 1..=size {
+            for num in 1..=size {
+                let idx = (row, col, num);
+                let name = format!("x_{row}_{col}_{num}");
+                x.insert(idx, problem.add(variable().binary().name(name)));
+            }
+        }
+    }
 
-    let objective = vec![1; size * size * size];
-    let mut b = vec![];
-    let mut constraints = vec![];
+    let gurobi =
+        GurobiSolver::new().command_name("/opt/gurobi1001/linux64/bin/gurobi_cl".to_string());
+    let solver = LpSolver(gurobi);
+    let mut model = problem.minimise(x.get(&(1, 1, 1)).unwrap()).using(solver);
 
-    // Each cell x_rc contains one value
-    for row in 0..size {
-        for col in 0..size {
-            let constraint: Vec<(usize, i64)> = (0..size).map(|v| (x(row, col, v), 1)).collect();
-            constraints.push(constraint);
-            b.push(Equality::EQ(1));
+    // Each cell x[r,c] contains one value
+    for row in 1..=size {
+        for col in 1..=size {
+            let mut eqn: good_lp::Expression = Expression::with_capacity(size);
+            for num in 1..=size {
+                eqn += x.get(&(row, col, num)).unwrap();
+            }
+            model.add_constraint(eqn.eq(1));
         }
     }
 
     // A value only appears once in each row
-    for row in 0..size {
-        for v in 0..size {
-            let constraint: Vec<(usize, i64)> = (0..size).map(|col| (x(row, col, v), 1)).collect();
-            constraints.push(constraint);
-            b.push(Equality::EQ(1));
+    for row in 1..size {
+        for num in 1..size {
+            let mut eqn: good_lp::Expression = Expression::with_capacity(size);
+            for col in 1..=size {
+                eqn += x.get(&(row, col, num)).unwrap();
+            }
+            model.add_constraint(eqn.eq(1));
         }
     }
 
     // A value only appears once in each col
-    for col in 0..size {
-        for v in 0..size {
-            let constraint: Vec<(usize, i64)> = (0..size).map(|row| (x(row, col, v), 1)).collect();
-            constraints.push(constraint);
-            b.push(Equality::EQ(1));
+    for col in 1..=size {
+        for num in 1..=size {
+            let mut eqn: good_lp::Expression = Expression::with_capacity(size);
+            for row in 1..=size {
+                eqn += x.get(&(row, col, num)).unwrap();
+            }
+            model.add_constraint(eqn.eq(1));
         }
     }
 
@@ -102,81 +110,31 @@ fn generate(puzzle: &str) -> Result<LpInfo> {
     for subgrid in 0..size {
         let start_row = subgrid / (size / box_cols) * box_rows;
         let start_col = subgrid % (size / box_cols) * box_cols;
-        for v in 0..size {
-            let mut constraint = vec![];
+        for num in 1..=size {
+            let mut eqn: good_lp::Expression = Expression::with_capacity(size);
             for r in 0..box_rows {
-                let row = r + start_row;
+                let row = r + start_row + 1;
                 for c in 0..box_cols {
-                    let col = c + start_col;
-                    constraint.push((x(row, col, v), 1));
+                    let col = c + start_col + 1;
+                    eqn += x.get(&(row, col, num)).unwrap();
                 }
             }
-            constraints.push(constraint);
-            b.push(Equality::EQ(1));
+            model.add_constraint(eqn.eq(1));
         }
     }
 
     // The original clues from the puzzle
-    let constraint: Vec<(usize, i64)> = puzzle
-        .chars()
-        .enumerate()
-        .filter(|(_, c)| *c != '0')
-        .map(|(i, c)| {
-            // We know this unwrap can't fail
-            let val = c.to_digit(10).unwrap() as usize;
-            let row = i / size;
-            let col = i % size;
-            (x(row, col, val - 1), 1)
-        })
-        .collect();
-
-    b.push(Equality::GE(constraint.len() as i64));
-    constraints.push(constraint);
-
-    Ok(LpInfo {
-        objective,
-        b,
-        constraints,
-    })
-}
-
-impl fmt::Display for LpInfo {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let vars = (0..self.objective.len())
-            .map(|i| format!("x{i}"))
-            .collect::<Vec<String>>();
-
-        writeln!(f, "Minimize\n0")?;
-        writeln!(f, "Subject To")?;
-        for (constraint, rhs) in self.constraints.iter().zip(&self.b) {
-            writeln!(f, "{}", constraint2eqn(constraint, rhs))?;
-        }
-        writeln!(f, "Binary")?;
-        writeln!(f, "{}", vars.join(" "))?;
-        writeln!(f, "End")
+    let mut eqn: good_lp::Expression = Expression::with_capacity(size * size);
+    let mut count = 0;
+    for (i, c) in puzzle.chars().enumerate().filter(|(_, c)| *c != '0') {
+        // We know this unwrap can't fail
+        let val = c.to_digit(10).unwrap() as usize;
+        let row = i / size + 1;
+        let col = i % size + 1;
+        eqn += x.get(&(row, col, val)).unwrap();
+        count += 1;
     }
-}
+    model.add_constraint(eqn.geq(count));
 
-fn constraint2eqn(constraint: &Vec<(usize, i64)>, rhs: &Equality) -> String {
-    let eqn = constraint
-        .iter()
-        .map(|(index, val)| format!("{val} x{index}"))
-        .collect::<Vec<String>>()
-        .join(" + ");
-    format!("{eqn} {rhs}")
-}
-
-enum Equality {
-    EQ(i64),
-    GE(i64),
-}
-
-impl fmt::Display for Equality {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let s = match self {
-            Equality::EQ(value) => format!(" = {value}"),
-            Equality::GE(value) => format!(">= {value}"),
-        };
-        write!(f, "{s}")
-    }
+    Ok(model)
 }
